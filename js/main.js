a85277da03fb7a4594f591fa0c355fb7869c88f6
rightnow <dldudsh83@gmail.com>
2022-02-01 20:03:13 +0900

    main menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-//검색 아이콘 클릭 핸들러. 검색창에 포커스 실행
 searchEl.addEventListener('click', function(){
   searchInputEl.focus();  
 });
 
-//검색창에 힌트 출력
 searchInputEl.addEventListener('focus', function(){
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
 })
 
-//포커스가 해제되면 힌트 삭제
 searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
