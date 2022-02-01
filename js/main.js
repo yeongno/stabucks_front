36006f401aca3f71f3c2a3f7d2c33cdececdd9d8
rightnow <dldudsh83@gmail.com>
2022-02-01 20:56:49 +0900

    badge: rodash.lib, gsap.lib

--- js/main.js
+++ js/main.js
@@ -8,9 +8,28 @@ searchEl.addEventListener('click', function(){
 searchInputEl.addEventListener('focus', function(){
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-})
+});
 
 searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+
+    gsap.to(badgeEl, .6, {
+      opacity:0,
+      display: 'none'
+    })
+  }else{
+    gsap.to(badgeEl, .6, {
+      opacity:1,
+      display:'block'
+    })
+  }
+},300));
+// _.throttle(함수, 시간)
