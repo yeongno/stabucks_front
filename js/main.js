eba3f204d8369514f215ed3fdeb5888b21a8603c
rightnow <dldudsh83@gmail.com>
2022-02-02 22:16:56 +0900

    scroll animation _ScrollMagic.lib

--- js/main.js
+++ js/main.js
@@ -103,3 +103,15 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+  .Scene({
+    triggerElement:spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: .8, //뷰포트의 y축의 비율 위부터 0
+  })
+  .setClassToggle(spyEl, 'show')
+  .addTo(new ScrollMagic.Controller());
+})
